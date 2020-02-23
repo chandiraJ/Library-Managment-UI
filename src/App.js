c2dfb270efb30ad913d5ca1b9ae2f4d1ee49cefe
Chandira Jayasekara <chandirajayasekara@gmail.com>
2020-02-23 21:58:04 +0530

    Before proceeding with Book functionalities

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import React from 'react';
 
+import {BrowserRouter as Router, Redirect, Route} from 'react-router-dom';
+// import {Route} from "react-router";
+
 import './App.css';
 import 'antd/dist/antd.css';
 
 import Login from './Components/Login/Login';
-import Layout from './Components/MainLayout/Layout';
+import MainLayout from './Components/MainLayout/Layout';
+
+// let logstat = sessionStorage.getItem("loging_status");
+let logstat = true;
+// if(logstat === "false"){
+//     logstat = true;
+// }
 
 function App() {
+    console.log(logstat)
+    function privateRoute() {
+        return logstat ? <Route  path={'/'} component={MainLayout}/> : <Redirect to={ '/login'}/>
+    }
     return (
         <div className="App">
-            <Layout/>
+            <Router>
+                { !logstat &&  <Route path={'/'} component={Login}/>}
+                <Route path={'/'} component={privateRoute}/>
+                {/*<Route path={'/'} component={MainLayout}/>*/}
+            </Router>
         </div>
     );
 }
--- src/Components/MainLayout/Layout.jsx
+++ src/Components/MainLayout/Layout.jsx
@@ -1,14 +1,27 @@
 import React, {Component} from 'react';
+import {Route, Switch} from "react-router";
 
 import Header from './Header';
 import Sider from './Sider';
+import {Layout} from "antd";
 
-export default class Layout extends Component {
+const {Content} = Layout;
+
+export default class LayoutX extends Component {
     render() {
         return <div>
             <Header/>
             <div className={'sider-n-content'}>
                 <Sider/>
+                <Content style={{width: '50%', minHeight: '700pt', padding: '10px', backgroundColor: '#fff'}}>
+
+                    <Switch>
+                        {/*<Route exact={true} path="/" component={Home}/>*/}
+                        {/*<Route path="/other" component={Documents}/>*/}
+                        {/*<Route path="/users" component={Users}/>*/}
+                    </Switch>
+                </Content>
+
             </div>
         </div>
     }
